9567cbfe3ea6c4ccfd794bc23c3a8bcafe741a30
Vipul Sonawane <vipulsonawane19071998@gmail.com>
2024-02-09 23:46:54 +0530

    new files added

--- src/App.js
+++ src/App.js
@@ -23,10 +23,10 @@ function App() {
        <Section4/>
        <Section5/>
        <Section6/>
-       <Section7/>
-       <Section8/>
-       <Section9/>
-       <Section10/>
+       {/* <Section7/> */}
+       {/* <Section8/> */}
+       {/* <Section9/> */}
+       {/* <Section10/> */}
     </div>
       
   );
--- src/components/Section8.js
+++ src/components/Section8.js
@@ -2,6 +2,7 @@ import "./Home.css"
 
 const Section8=()=>{
   const video = document.getElementsByClassName("video");
+  const faqs = document.querySelectorAll("divfaq-box-question");
   let i;
   for (i = 0; i < video.length; i++) {
     video[i].addEventListener("click", function () {
@@ -32,7 +33,14 @@ const Section8=()=>{
         controls.innerHTML = "▶";
       },
       false
-    );  
+    ); 
+
+    faqs.forEach((faq)=>{
+      faq.addEventListener("click",()=>{
+        faq.classList.toggle("active");
+      });
+    });
+
   }
     return(
     <>
@@ -96,33 +104,109 @@ const Section8=()=>{
               efforts reach those most likely to engage with your brand.
             </div>
           </div>
+          
           <div className="faq">
             <div className="divfaq-box-question">
-              <div className="div">
-                <div className="how-does-osumare">
+              {/* <div className="div"> */}
+              <div className="how-does-osumare">
                   1. How does Osumare measure campaign success?
-                </div>
-                <div className="ifa-solid">
-                  {/* <button className="symbol1">
+              </div>
+                    {/* <div className="ifa-solid"> */}
+                    {/* <button className="symbol1">
                     {/* <img alt="" src="i.fa-solid.png"/> */}
                     {/* <i class="fa-solid fa-chevron-down"></i> */}
-                  {/* </button> } */}
-                 
-                </div>
-                <svg path="/i.fa-solid.svg"/>
-              </div>
+                    {/* </button> */} 
+                   
+                    <svg width="14" height="20" viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.41406 14.2891L12.7266 8.97656C13.1172 8.58594 13.1172 8 12.7266 7.64844L11.8672 6.75C11.4766 6.39844 10.8906 6.39844 10.5391 6.75L6.78906 10.5391L3 6.75C2.64844 6.39844 2.0625 6.39844 1.67188 6.75L0.8125 7.64844C0.421875 8 0.421875 8.58594 0.8125 8.97656L6.125 14.2891C6.47656 14.6406 7.0625 14.6406 7.41406 14.2891Z" fill="#010103"/>
+                </svg>
+                  
+               
+                {/* </div> */}
+               
+                {/* </div> */}
             </div>
-            <div className="divfaq-box">
+            <div className="answer">
+              <p>
+                We believe in measurable results. Our data-driven approach
+                  means that every campaign's performance is tracked, analyzed,
+                  and refined for optimal outcomes. We provide regular reports
+                  that detail key metrics, giving you clear insights into how
+                  our strategies are driving growth for your brand.
+              </p>
+            </div>
+          </div>
+          <div className="faq">
+            <div className="divfaq-box-question">
+              {/* <div className="div"> */}
+              <div className="how-does-osumare">
+                2. How does Osumare measure campaign success?
+              </div>
+                    {/* <div className="ifa-solid"> */}
+                    {/* <button className="symbol1">
+                    {/* <img alt="" src="i.fa-solid.png"/> */}
+                    {/* <i class="fa-solid fa-chevron-down"></i> */}
+                    {/* </button> */} 
+                    
+                <svg width="14" height="20" viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.41406 14.2891L12.7266 8.97656C13.1172 8.58594 13.1172 8 12.7266 7.64844L11.8672 6.75C11.4766 6.39844 10.8906 6.39844 10.5391 6.75L6.78906 10.5391L3 6.75C2.64844 6.39844 2.0625 6.39844 1.67188 6.75L0.8125 7.64844C0.421875 8 0.421875 8.58594 0.8125 8.97656L6.125 14.2891C6.47656 14.6406 7.0625 14.6406 7.41406 14.2891Z" fill="#010103"/>
+                </svg>
+                {/* </div> */}
+               
+                {/* </div> */}
+            </div>
+            <div className="answer">
+              <p>
+                We believe in measurable results. Our data-driven approach
+                  means that every campaign's performance is tracked, analyzed,
+                  and refined for optimal outcomes. We provide regular reports
+                  that detail key metrics, giving you clear insights into how
+                  our strategies are driving growth for your brand.
+              </p>
+            </div>
+          </div>
+          <div className="faq">
+            <div className="divfaq-box-question">
+              {/* <div className="div"> */}
+              <div className="how-does-osumare">
+                3. How does Osumare measure campaign success?
+              </div>
+                    {/* <div className="ifa-solid"> */}
+                    {/* <button className="symbol1">
+                    {/* <img alt="" src="i.fa-solid.png"/> */}
+                    {/* <i class="fa-solid fa-chevron-down"></i> */}
+                    {/* </button> */} 
+                <svg width="14" height="20" viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.41406 14.2891L12.7266 8.97656C13.1172 8.58594 13.1172 8 12.7266 7.64844L11.8672 6.75C11.4766 6.39844 10.8906 6.39844 10.5391 6.75L6.78906 10.5391L3 6.75C2.64844 6.39844 2.0625 6.39844 1.67188 6.75L0.8125 7.64844C0.421875 8 0.421875 8.58594 0.8125 8.97656L6.125 14.2891C6.47656 14.6406 7.0625 14.6406 7.41406 14.2891Z" fill="#010103"/>
+                </svg>
+                {/* </div> */}
+               
+                {/* </div> */}
+            </div>
+            <div className="answer">
+              <p>
+                We believe in measurable results. Our data-driven approach
+                  means that every campaign's performance is tracked, analyzed,
+                  and refined for optimal outcomes. We provide regular reports
+                  that detail key metrics, giving you clear insights into how
+                  our strategies are driving growth for your brand.
+              </p>
+            </div>
+          </div>
+            {/* <div className="divfaq-box">
               <div className="divfaq-box-question1">
                 <div className="how-does-osumare-measure-campa-parent">
                   <div className="how-does-osumare">
                     2. How does Osumare measure campaign success?
                   </div>
-                  <div className="ifa-solid">
+                  {/* <div className="ifa-solid">
                     <div className="symbol1">
                     <img alt="" src="i.fa-solid.png"/>
                     </div>
-                  </div>
+                  </div> }
+                  <svg width="14" height="20" viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7.41406 14.2891L12.7266 8.97656C13.1172 8.58594 13.1172 8 12.7266 7.64844L11.8672 6.75C11.4766 6.39844 10.8906 6.39844 10.5391 6.75L6.78906 10.5391L3 6.75C2.64844 6.39844 2.0625 6.39844 1.67188 6.75L0.8125 7.64844C0.421875 8 0.421875 8.58594 0.8125 8.97656L6.125 14.2891C6.47656 14.6406 7.0625 14.6406 7.41406 14.2891Z" fill="#010103"/>
+                  </svg>
                 </div>
                 <div className="p1" />
               </div>
@@ -133,11 +217,14 @@ const Section8=()=>{
                   <div className="how-does-osumare2">
                     3. How does Osumare measure campaign success?
                   </div>
-                  <div className="ifa-solid">
+                  {/* <div className="ifa-solid">
                     <div className="symbol3">
                     <img alt="" src="i.fa-solid.png"/>
                     </div>
-                  </div>
+                  </div> }
+                  <svg width="14" height="20" viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7.41406 14.2891L12.7266 8.97656C13.1172 8.58594 13.1172 8 12.7266 7.64844L11.8672 6.75C11.4766 6.39844 10.8906 6.39844 10.5391 6.75L6.78906 10.5391L3 6.75C2.64844 6.39844 2.0625 6.39844 1.67188 6.75L0.8125 7.64844C0.421875 8 0.421875 8.58594 0.8125 8.97656L6.125 14.2891C6.47656 14.6406 7.0625 14.6406 7.41406 14.2891Z" fill="#010103"/>
+                  </svg>
                 </div>
                 <div className="p2" />
               </div>
@@ -151,7 +238,20 @@ const Section8=()=>{
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
+         
+          {/* <div className="faq">
+            <div className="question">
+              <h3>
+              1. How does Osumare measure campaign success?
+              </h3>
+              <svg width="14" height="20" viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7.41406 14.2891L12.7266 8.97656C13.1172 8.58594 13.1172 8 12.7266 7.64844L11.8672 6.75C11.4766 6.39844 10.8906 6.39844 10.5391 6.75L6.78906 10.5391L3 6.75C2.64844 6.39844 2.0625 6.39844 1.67188 6.75L0.8125 7.64844C0.421875 8 0.421875 8.58594 0.8125 8.97656L6.125 14.2891C6.47656 14.6406 7.0625 14.6406 7.41406 14.2891Z" fill="#010103"/>
+              </svg>
+
+
+            </div>
+          </div> */}
         </div>
       </div>
 
